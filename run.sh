32280d19a3335a75f259cb5a7b949a6687131774
xySaad <saadrm.dev@gmail.com>
2025-12-29 03:54:19 +0100

    cleanup

--- run.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.config/systemd/user
-
-echo '[Service]
-ExecStart=/usr/bin/chisel client http://01auth.undo.it:25565 R:25555:localhost:25566
-Restart=always
-
-[Install]
-WantedBy=default.target' > ~/.config/systemd/user/01proxy.service
